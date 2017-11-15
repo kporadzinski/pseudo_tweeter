fd385bbc0b25af291da218934d0a6fae94f5abcb
acme <acme@acme-Lenovo-ideapad-310-15ISK>
2017-11-15 23:20:30 +0100

    more magic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function noc(i) {
 
-  var formy = document.getElementById('form12');
+  var formy = document.getElementById('form'+i);
   console.log('i wynosi'+i);
   formy.classList.toggle('noc');
   return (formy)
